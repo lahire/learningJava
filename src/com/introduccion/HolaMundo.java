d8a6602f7620868fa2d7b12b9bfdec408ebca7df
Cacho <niko.kane@gmail.com>
2023-09-16 13:34:59 -0300

    cosas

---
+++ src/com/introduccion/HolaMundo.java
@@ -0,0 +1,11 @@
+package src.com.introduccion;
+
+public class HolaMundo {
+  public static void main(String[] args) {
+    System.out.println("¡Hola que tal");
+  }
+
+  public static String saludar() {
+    return "Hola!";
+  }
+}
--- src/com/trioptimum/Programa.java
+++
@@ -1,7 +0,0 @@
-package src.com.trioptimum;
-
-public class Programa {
-  public static void main(String[] args) {
-    System.out.println("¡Hola que tal");
-  }
-}
